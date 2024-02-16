04f796bd1c6c771877f5ed84c7e145949e9ae31e
aman <amanagarwalx123@gmail.com>
2024-02-16 12:16:46 +0530

    changes

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,5 +1,5 @@
 
-export default function TripCard({ id, title, totalCount, filledCount, price, baseLocation, days }) {
+export default function TripCard({ id, title, totalCount, filledCount, price, baseLocation, days }: any) {
     return (
         <div className="h-40 mt-8 max-w-[1000px] border border-black border-opacity-5 shadow-sm rounded-xl mx-auto">
 
